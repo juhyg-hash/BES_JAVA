5b56359ce67465ffb1e62b3a9fc041e5d87848ed
juhyg-hash <gusrb1474@gmail.com>
2025-05-30 14:36:48 +0900

    Variables

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,19 @@ public class Main {
         int age = 25;
 
         System.out.println(age);
+
+
+        age = 26;
+
+        System.out.println(age);
+
+        double salary = 5000.74;
+        System.out.println(salary);
+
+        String name = "주현규";
+
+        System.out.println(name);
+        System.out.println(name.length());
+
     }
 }
